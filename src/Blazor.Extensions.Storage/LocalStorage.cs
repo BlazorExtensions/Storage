b6985900627fc5f484a97693de0a5df6c78cf259
Joe Cellucci <celluj34@gmail.com>
2019-11-14 22:35:38 -0600

    update LocalStorage and SessionStorage to use new base class

--- src/Blazor.Extensions.Storage/LocalStorage.cs
+++ src/Blazor.Extensions.Storage/LocalStorage.cs
@@ -1,44 +1,10 @@
 using Blazor.Extensions.Storage.Interfaces;
 using Microsoft.JSInterop;
-using System;
-using System.Threading.Tasks;
 
 namespace Blazor.Extensions.Storage
 {
-    internal class LocalStorage : ILocalStorage
+    internal class LocalStorage : Storage, ILocalStorage
     {
-        private readonly IJSRuntime runtime;
-
-        public ValueTask<int> Length() => this.runtime.InvokeAsync<int>(MethodNames.LENGTH_METHOD, StorageTypeNames.LOCAL_STORAGE);
-
-        public ValueTask Clear() => this.runtime.InvokeVoidAsync(MethodNames.CLEAR_METHOD, StorageTypeNames.LOCAL_STORAGE);
-
-        public LocalStorage(IJSRuntime runtime)
-        {
-            this.runtime = runtime;
-        }
-
-        public ValueTask<TItem> GetItem<TItem>(string key)
-        {
-            if (string.IsNullOrWhiteSpace(key)) throw new ArgumentNullException(nameof(key));
-
-            return this.runtime.InvokeAsync<TItem>(MethodNames.GET_ITEM_METHOD, StorageTypeNames.LOCAL_STORAGE, key);
-        }
-
-        public ValueTask<string> Key(int index) => this.runtime.InvokeAsync<string>(MethodNames.KEY_METHOD, StorageTypeNames.LOCAL_STORAGE, index);
-
-        public ValueTask RemoveItem(string key)
-        {
-            if (string.IsNullOrWhiteSpace(key)) throw new ArgumentNullException(nameof(key));
-
-            return this.runtime.InvokeVoidAsync(MethodNames.REMOVE_ITEM_METHOD, StorageTypeNames.LOCAL_STORAGE, key);
-        }
-
-        public ValueTask SetItem<TItem>(string key, TItem item)
-        {
-            if (string.IsNullOrWhiteSpace(key)) throw new ArgumentNullException(nameof(key));
-
-            return this.runtime.InvokeVoidAsync(MethodNames.SET_ITEM_METHOD, StorageTypeNames.LOCAL_STORAGE, key, item);
-        }
+        public LocalStorage(IJSRuntime runtime) : base(runtime, StorageTypeNames.LOCAL_STORAGE) {}
     }
 }
