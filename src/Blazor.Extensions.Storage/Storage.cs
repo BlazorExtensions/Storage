e691e692ca25a5eb749a7bd31760e11eca8f6296
Joe Cellucci <celluj34@gmail.com>
2019-11-14 22:38:21 -0600

    move the method constants to inside the Storage base class

--- src/Blazor.Extensions.Storage/Storage.cs
+++ src/Blazor.Extensions.Storage/Storage.cs
@@ -42,5 +42,15 @@ namespace Blazor.Extensions.Storage
 
             return this.runtime.InvokeVoidAsync(MethodNames.SET_ITEM_METHOD, this.storageName, key, item);
         }
+
+        private class MethodNames
+        {
+            public const string LENGTH_METHOD = "BlazorExtensions.Storage.Length";
+            public const string KEY_METHOD = "BlazorExtensions.Storage.Key";
+            public const string GET_ITEM_METHOD = "BlazorExtensions.Storage.GetItem";
+            public const string SET_ITEM_METHOD = "BlazorExtensions.Storage.SetItem";
+            public const string REMOVE_ITEM_METHOD = "BlazorExtensions.Storage.RemoveItem";
+            public const string CLEAR_METHOD = "BlazorExtensions.Storage.Clear";
+        }
     }
 }
